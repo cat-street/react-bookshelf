c24584b3f65f2388b3f702ff960269ac0b059541
Andrey Kudryavtsev <lomastering@gmail.com>
2021-05-30 00:09:15 +0300

    fix: mirage

--- src/index.tsx
+++ src/index.tsx
@@ -4,32 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import makeServer from './mock/mirage';
 
-// import { createStore, compose, combineReducers } from 'redux';
-// import booksReducer from './store/reducers/books';
-// import authReducer from './store/reducers/auth';
-
 import store from './store/store';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-if (process.env.NODE_ENV === 'development') {
-  makeServer({ environment: 'development' });
-}
-// declare global {
-//   interface Window {
-//     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-//   }
-// }
-
-// const rootReducer = combineReducers({
-//   books: booksReducer,
-//   auth: authReducer,
-// });
-
-// const composeEnhancers = process.env.NODE_ENV === 'development'
-//   ? (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ as typeof compose)
-//   : null || compose;
-// const store = createStore(rootReducer, composeEnhancers());
+makeServer({ environment: 'development' });
 
 ReactDOM.render(
   <React.StrictMode>
